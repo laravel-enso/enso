49f5d1a7ce917f9723e4d0395360802dfade229d
mishu <mishu@xtelecom.ro>
2017-07-08 00:15:00 +0300

    update to 1.1.30

--- config/laravel-enso.php
+++ config/laravel-enso.php
@@ -1,7 +1,7 @@
 <?php
 
     return [
-        'version'             => '1.1.28',
+        'version'             => '1.1.30',
         'defaultRole'         => 'admin',
         'formattedTimestamps' => 'd-m-Y',
         'paths'               => [
