a0cac1b6d4463721dad5217430fd4df8c06bca27
Adrian Ocneanu <aocneanu@gmail.com>
2017-11-23 20:33:16 +0000

    Apply fixes from StyleCI

--- config/enso/config.php
+++ config/enso/config.php
@@ -1,16 +1,16 @@
 <?php
 
 return [
-    'version' => '2.1.0',
-    'defaultRole' => 'admin',
-    'defaultRole' => 'admin',
+    'version'       => '2.1.0',
+    'defaultRole'   => 'admin',
+    'defaultRole'   => 'admin',
     'phpDateFormat' => 'd-m-Y',
-    'jsDateFormat' => 'DD-MM-YYYY',
-    'paths' => [
-        'files' => 'files',
+    'jsDateFormat'  => 'DD-MM-YYYY',
+    'paths'         => [
+        'files'   => 'files',
         'avatars' => 'avatars',
         'imports' => 'imports',
-        'temp' => 'temp',
+        'temp'    => 'temp',
         'exports' => 'exports',
     ],
     'cacheLifetime' => env('CACHE_LIFETIME', 60),
