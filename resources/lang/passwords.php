dde34d568467a178261c66f5741347ab22f54163
Delgersuren <shonhor22@yahoo.com>
2018-03-07 21:07:49 +0800

    mongolian language added

---
+++ resources/lang/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => 'Нууц үг багадаа 6 тэмдэгттэй байх ба давтан оруулсан нууц үгс хоорондоо ижил байх шаардлагатай.',
+    'reset' => 'Нууц үг солигдлоо!',
+    'sent' => 'Нууц үг солих линк хаягыг таны и-мэйл хаягаар илгээлээ!',
+    'token' => 'Нууц үг солих линк буруу байна.',
+    'user' => "Ийм и-мэйл хаягтай хэрэглэгч бүртгэлгүй байна.",
+
+];
