573987ebefaadf1b15c37b9106b7f670dc48ebdb
mishu <mishu@xtelecom.ro>
2018-08-16 11:15:44 +0300

    removes sql options

--- config/database.php
+++ config/database.php
@@ -74,10 +74,6 @@ return [
             'password' => env('DB_PASSWORD', ''),
             'charset' => 'utf8',
             'prefix' => '',
-            'options' => [
-              PDO::ATTR_STRINGIFY_FETCHES => false,
-              PDO::SQLSRV_ATTR_FETCHES_NUMERIC_TYPE => true,
-            ],
         ],
     ],
 
