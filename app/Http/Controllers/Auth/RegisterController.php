f3aee40e0e2e7b924b21942c44cdfe340ed8b3df
Adrian Ocneanu <aocneanu@gmail.com>
2018-09-04 15:32:36 +0000

    Apply fixes from StyleCI

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -44,6 +44,7 @@ class RegisterController extends Controller
      * Get a validator for an incoming registration request.
      *
      * @param  array  $data
+     *
      * @return \Illuminate\Contracts\Validation\Validator
      */
     protected function validator(array $data)
@@ -59,6 +60,7 @@ class RegisterController extends Controller
      * Create a new user instance after a valid registration.
      *
      * @param  array  $data
+     *
      * @return \App\User
      */
     protected function create(array $data)
