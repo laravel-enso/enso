a405ad3c36b80d4ffa8d8e5f268812cac47467de
Jonas De Kegel <jonas@jlss.eu>
2019-02-05 15:41:41 +0100

    Fix & Update broadcasting.php config
    Part of https://github.com/laravel-enso/Notifications/issues/5 (laravel-websocket support)
    'localpusher' is a performance-optimizing local push which doesn't require SSL (when using laravel-websockets
    
    Note that server-side "encrypted" should be "scheme" (see [docs](https://github.com/pusher/pusher-http-php#pusher-constructor))

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -34,8 +34,23 @@ return [
             'secret' => env('PUSHER_APP_SECRET'),
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
-              'cluster' => env('PUSHER_APP_CLUSTER'),
-              'encrypted' => true,
+                'host' => env('PUSHER_APP_HOST', 'api.pusherapp.com'),
+                'port' => env('PUSHER_APP_PORT'),
+                'cluster' => env('PUSHER_APP_CLUSTER'),
+                'scheme' => env('PUSHER_APP_SCHEME', 'https'),
+            ],
+        ],
+
+        'localpusher' => [
+            'driver' => 'pusher',
+            'key' => env('PUSHER_APP_KEY'),
+            'secret' => env('PUSHER_APP_SECRET'),
+            'app_id' => env('PUSHER_APP_ID'),
+            'options' => [
+                'host' => '127.0.0.1',
+                'port' => 6001,
+                'cluster' => env('PUSHER_APP_CLUSTER'),
+                'scheme' => 'http',
             ],
         ],
 
