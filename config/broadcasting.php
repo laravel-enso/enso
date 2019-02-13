2f57088adb638f72dd9e1a78244c2eaae728e708
Adrian Ocneanu <aocneanu@gmail.com>
2019-02-13 08:32:32 +0200

    Update broadcasting.php

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -37,20 +37,7 @@ return [
                 'host' => env('PUSHER_APP_HOST', 'api.pusherapp.com'),
                 'port' => env('PUSHER_APP_PORT'),
                 'cluster' => env('PUSHER_APP_CLUSTER'),
-                'scheme' => env('PUSHER_APP_SCHEME', 'https'),
-            ],
-        ],
-
-        'localpusher' => [
-            'driver' => 'pusher',
-            'key' => env('PUSHER_APP_KEY'),
-            'secret' => env('PUSHER_APP_SECRET'),
-            'app_id' => env('PUSHER_APP_ID'),
-            'options' => [
-                'host' => '127.0.0.1',
-                'port' => 6001,
-                'cluster' => env('PUSHER_APP_CLUSTER'),
-                'scheme' => 'http',
+                'encrypted' => true,
             ],
         ],
 
