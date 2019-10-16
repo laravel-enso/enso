a9f0947e53154f37509240b26b22a83e3e3d3b82
Adrian Ocneanu <aocneanu@gmail.com>
2019-10-16 09:39:23 +0000

    Apply fixes from StyleCI

--- app/Providers/LoggerServiceProvider.php
+++ app/Providers/LoggerServiceProvider.php
@@ -29,7 +29,7 @@ class LoggerServiceProvider extends ServiceProvider
     {
         parent::boot();
 
-        if (! App::environment('testing')) {
+        if (!App::environment('testing')) {
             Logger::observe();
         }
     }
