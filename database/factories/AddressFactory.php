eb930eb773a6efc3db376cfdcc0fec8c65b04228
reza <raftx24@gmail.com>
2019-10-24 16:11:21 +0330

    fix country_id

--- database/factories/AddressFactory.php
+++ database/factories/AddressFactory.php
@@ -2,12 +2,13 @@
 
 use Faker\Generator as Faker;
 use LaravelEnso\Addresses\app\Models\Address;
+use LaravelEnso\Addresses\app\Models\Country;
 
 $factory->define(Address::class, function (Faker $faker) {
     return [
         'addressable_id' => $faker->randomKey,
         'addressable_type' => $faker->word,
-        'country_id' => $faker->randomDigit,
+        'country_id' => Country::inRandomOrder()->first()->id,
         'apartment' => $faker->word,
         'street' => $faker->streetAddress,
         'city' => $faker->city,
