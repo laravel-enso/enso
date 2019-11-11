83bb8dd867a97096a6cff37e517bdbe699eea5a8
mishu <mishu@xtelecom.ro>
2019-11-11 13:13:14 +0200

    updates babel.config.js

--- client/babel.config.js
+++ client/babel.config.js
@@ -1,5 +1,12 @@
 module.exports = {
     presets: [
-        '@vue/cli-plugin-babel/preset',
+        [
+            '@vue/cli-plugin-babel/preset',
+            {
+                targets: { esmodules: true },
+                polyfills: [],
+            },
+        ],
     ],
 };
+
