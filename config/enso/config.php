35663ae0f268f63013bb22b8c24c986085725f9a
Mihai <mihai.ocneanu@gmail.com>
2019-11-19 17:05:47 +0200

    updated the enso/config.php

--- config/enso/config.php
+++ config/enso/config.php
@@ -2,7 +2,8 @@
 
 return [
     'version' => '3.7.1',
-    'showQuote' => true,
+    'ownerCompanyId' => env('OWNER_COMPANY_ID', 5830),
+    'showQuote' => env('SHOW_QUOTE', true),
     'defaultRole' => 'admin',
     'dateFormat' => 'd-m-Y',
     'dateTimeFormat' => 'd-m-Y H:i:s',
@@ -12,5 +13,5 @@ return [
     'ravenKey' => env('RAVEN_DSN', null),
     'cacheLifetime' => env('CACHE_LIFETIME', 60),
     'ensoApiToken' => env('ENSO_API_TOKEN', null),
-    'extendedDocumentTitle' => false,
+    'extendedDocumentTitle' => env('EXTENDED_DOCUMENT_TITLE', false),
 ];
