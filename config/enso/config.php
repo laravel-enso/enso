95e69bd61e444fde9b655ce98486d3a43a3d2df6
Mihai <mihai.ocneanu@gmail.com>
2020-03-30 14:04:29 +0300

    dependencies update

--- config/enso/config.php
+++ config/enso/config.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'version' => '3.9.0',
+    'version' => '3.9.1',
     'ownerCompanyId' => env('OWNER_COMPANY_ID', 1),
     'showQuote' => env('SHOW_QUOTE', true),
     'defaultRole' => 'admin',
