dc5cb2427f41494a1ee26d8214aec3e7d98ff3e8
reza <raftx24@gmail.com>
2020-07-01 11:09:09 +0430

    adds dynamic relations to company

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,9 +2,14 @@
 
 namespace App\Providers;
 
+use App\DynamicRelations\Company\Comments;
+use App\DynamicRelations\Company\Discussions;
+use App\DynamicRelations\Company\Documents;
 use App\Models\User;
 use Illuminate\Support\ServiceProvider;
+use LaravelEnso\Companies\Models\Company;
 use LaravelEnso\Core\Models\User as BaseUser;
+use LaravelEnso\DynamicMethods\Services\Methods;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -18,5 +23,6 @@ class AppServiceProvider extends ServiceProvider
 
     public function register()
     {
+        Methods::bind(Company::class, [Comments::class, Discussions::class, Documents::class]);
     }
 }
