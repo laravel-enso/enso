27aa3a056419d0ffaa2fa3741cf245c6b437990a
reza <raftx24@gmail.com>
2020-11-03 15:29:20 +0330

    improves dynamic packages

--- client/src/js/enso.js
+++ client/src/js/enso.js
@@ -3,11 +3,11 @@ import Vue from 'vue';
 import '@core/modules';
 
 import { sync } from 'vuex-router-sync';
-import App from '@enso-ui/ui/bulma';
-// eslint-disable-next-line import/no-extraneous-dependencies,import/extensions
-import tasksInitializer from '@enso-ui/tasks/src/index.js';
+import AppComponent from '@enso-ui/ui/bulma';
+import App from '@enso-ui/ui/src/bulma/app';
 import store from './store';
 import router from './router';
+import '@enso-ui/ui/src/bulma/boot';
 import './app';
 
 import '../sass/enso.scss';
@@ -19,7 +19,7 @@ Vue.config.productionTip = false;
 const root = new Vue({
     router,
     store,
-    ...App,
+    ...AppComponent,
 }).$mount('#app');
 
-tasksInitializer(root);
+App.boot(root);
