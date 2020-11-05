9c5f8f874045fb65307357e89cd6761e75e9810d
reza <raftx24@gmail.com>
2020-11-05 18:48:45 +0330

    adds auto discovery

--- client/src/js/app.js
+++
@@ -1,4 +0,0 @@
-import '@calendar/icons';
-import '@howTo/icons';
-import '@activityLog/icons';
-import '@tutorials/icons';
--- client/src/js/router.js
+++ client/src/js/router.js
@@ -1,22 +1,13 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import routeImporter from '@core/modules/importers/routeImporter';
-import RouteMerger from '@core/modules/importers/RouteMerger';
 import before from '@core/middleware/before';
-import routes from '@core/bulma/routes';
 
 Vue.use(Router);
 
-(new RouteMerger(routes))
-    .add(routeImporter(require.context('@calendar/routes', false, /.*\.js$/)))
-    .add(routeImporter(require.context('@howTo/routes', false, /.*\.js$/)))
-    .add(routeImporter(require.context('@activityLog/routes', false, /.*\.js$/)))
-    .add(routeImporter(require.context('@tutorials/routes', false, /.*\.js$/)))
-    .add(routeImporter(require.context('./routes', false, /.*\.js$/)));
-
 const router = new Router({
     mode: 'history',
-    routes,
+    routes: routeImporter(require.context('./routes', false, /.*\.js$/)),
     scrollBehavior(to, from, savedPosition) {
         return savedPosition || { x: 0, y: 0 };
     },
