4d66e4db428882fd2830ff555bcfaac75509da95
reza <raftx24@gmail.com>
2020-11-06 12:01:36 +0330

    fixes routes auto discovery

--- client/src/js/router.js
+++ client/src/js/router.js
@@ -2,12 +2,16 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import routeImporter from '@core/modules/importers/routeImporter';
 import before from '@core/middleware/before';
+import App from '@enso-ui/ui/src/core/app';
 
 Vue.use(Router);
 
+const { routes } = App.getRoutes('bulma')
+    .add(routeImporter(require.context('./routes', false, /.*\.js$/)));
+
 const router = new Router({
     mode: 'history',
-    routes: routeImporter(require.context('./routes', false, /.*\.js$/)),
+    routes,
     scrollBehavior(to, from, savedPosition) {
         return savedPosition || { x: 0, y: 0 };
     },
