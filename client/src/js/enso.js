cebef8aebec412a6684b713c37d6476d6b5e0c2d
Mihai <mihai.ocneanu@gmail.com>
2020-11-10 16:36:27 +0200

    more changes as a result of UI refactor

--- client/src/js/enso.js
+++ client/src/js/enso.js
@@ -1,24 +1,16 @@
 import Vue from 'vue';
-
-import '@core/modules';
-
 import { sync } from 'vuex-router-sync';
 import Root from '@enso-ui/ui/src/bulma/Root.vue';
-import App from '@enso-ui/ui/src/core/bulma';
-import router from './router';
-import store from './store';
-
+import App from '@enso-ui/ui/src/core/app';
+import router from '@enso-ui/ui/src/core/services/router';
+import store from '@enso-ui/ui/src/core/services/store';
+import '@enso-ui/ui/src/modules';
 import './app';
-
 import '../sass/enso.scss';
 
-const routes = App.buildRoutes(require.context('./routes', false, /.*\.js$/));
-
-router.addRoutes(routes);
+Vue.config.productionTip = false;
 
 sync(store, router);
-
-Vue.config.productionTip = false;
 
 const vm = new Vue({
     router,
