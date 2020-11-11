7e3662b024277b46304ef25e13e8ce7cb4e466ec
Mihai <mihai.ocneanu@gmail.com>
2020-11-11 10:33:56 +0200

    removed bus usage

--- client/src/js/localState.js
+++ client/src/js/localState.js
@@ -1,6 +1,3 @@
 window.addEventListener('local-state-fetched', ({ detail }) => {
     console.log(detail);
 });
-
-// eslint-disable-next-line
-Bus.subscribe('stateFetched', ({ context, state }) => {});
