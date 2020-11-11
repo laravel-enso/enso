ac5e791ce3d2736bae8c96b17620b77234ee95bb
Mihai <mihai.ocneanu@gmail.com>
2020-11-11 10:28:49 +0200

    merged

--- client/src/js/enso.js
+++ client/src/js/enso.js
@@ -5,9 +5,9 @@ import App from '@enso-ui/ui/src/core/app';
 import router from '@enso-ui/ui/src/core/services/router';
 import store from '@enso-ui/ui/src/core/services/store';
 import '@enso-ui/ui/src/modules';
-import './localState';
 import './app';
 import '../sass/enso.scss';
+import './localState';
 
 Vue.config.productionTip = false;
 
--- client/src/js/localState.js
+++ client/src/js/localState.js
@@ -1,4 +1,10 @@
+<<<<<<< Updated upstream
 import Bus from '@enso-ui/ui/src/core/bus';
+=======
+window.addEventListener('local-state-fetched', ({ detail }) => {
+    console.log(detail);
+});
+>>>>>>> Stashed changes
 
 // eslint-disable-next-line
 Bus.subscribe('stateFetched', ({ context, state }) => {});
