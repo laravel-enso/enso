aaba31508b96c28d7adb2b30a81771812270c9f4
Mihai <mihai.ocneanu@gmail.com>
2020-11-11 10:33:30 +0200

    fixed merge issue

--- client/src/js/localState.js
+++ client/src/js/localState.js
@@ -1,10 +1,6 @@
-<<<<<<< Updated upstream
-import Bus from '@enso-ui/ui/src/core/bus';
-=======
 window.addEventListener('local-state-fetched', ({ detail }) => {
     console.log(detail);
 });
->>>>>>> Stashed changes
 
 // eslint-disable-next-line
 Bus.subscribe('stateFetched', ({ context, state }) => {});
