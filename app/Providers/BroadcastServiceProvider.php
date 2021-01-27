6e6f14c5e1664765ca05d186826c69d1fac5fc0b
Reza Fouladi <raftx24@gmail.com>
2021-01-27 23:47:53 +1100

    Upgrade/4.6.0 extra (#369)
    * removes `Broadcast::routes()` from BroadcastServiceProvider
    
    * updates changelog

--- app/Providers/BroadcastServiceProvider.php
+++ app/Providers/BroadcastServiceProvider.php
@@ -2,15 +2,12 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\Broadcast;
 use Illuminate\Support\ServiceProvider;
 
 class BroadcastServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        Broadcast::routes();
-
         require base_path('routes/channels.php');
     }
 }
