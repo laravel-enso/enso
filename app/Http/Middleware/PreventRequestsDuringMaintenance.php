061060f917e56b11d3e27b45f4368513ca08ad46
mishu <mishu@xtelecom.ro>
2021-02-13 12:39:50 +0200

    fixes GA

--- app/Http/Middleware/PreventRequestsDuringMaintenance.php
+++ app/Http/Middleware/PreventRequestsDuringMaintenance.php
@@ -6,12 +6,5 @@ use Illuminate\Foundation\Http\Middleware\PreventRequestsDuringMaintenance as Mi
 
 class PreventRequestsDuringMaintenance extends Middleware
 {
-    /**
-     * The URIs that should be reachable while maintenance mode is enabled.
-     *
-     * @var array
-     */
-    protected $except = [
-        //
-    ];
+    protected $except = [];
 }
