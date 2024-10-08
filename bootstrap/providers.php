911aefb65047d3159d54c247f4d3f88f5e635fc7
Vlad Chiovianu <vlad.chiovianu@earthlink.ro>
2024-10-08 17:38:01 +0300

    adds missing providers

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,4 +2,7 @@
 
 return [
     App\Providers\AppServiceProvider::class,
+    App\Providers\CalendarServiceProvider::class,
+    App\Providers\HorizonServiceProvider::class,
+    App\Providers\TelescopeServiceProvider::class,
 ];
