a3d4a76a602cf341adf2d52afe9ecbc7a730e1c9
Vlad Chiovianu <vlad.chiovianu@earthlink.ro>
2024-10-08 17:36:44 +0300

    removes leftovers

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -19,8 +19,6 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
-        health: '/up',
     )
     ->withMiddleware(fn (Middleware $middleware) => $middleware
         ->use($globalMiddlaware)
