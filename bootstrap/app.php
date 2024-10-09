9f6f018462b6ed67c6e56ce53af602844f8d9207
Vlad Chiovianu <vlad.chiovianu@earthlink.ro>
2024-10-09 11:12:02 +0300

    prepares

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -19,6 +19,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         api: __DIR__.'/../routes/api.php',
+        commands: __DIR__.'/../routes/console.php',
     )
     ->withMiddleware(fn (Middleware $middleware) => $middleware
         ->use($globalMiddlaware)
