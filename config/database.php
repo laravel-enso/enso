2848a726d6609a8a26393ed63dd16c0cf48eb6ad
Vlad Chiovianu <vlad.chiovianu@earthlink.ro>
2024-10-09 17:40:12 +0300

    still debugging

--- config/database.php
+++ config/database.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'sq_lite'),
+    'default' => env('DB_CONNECTION', 'mysql'),
 
     /*
     |--------------------------------------------------------------------------
